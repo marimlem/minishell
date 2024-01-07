d8eccdfdf7f4ab4124abe43900a161f0cf50fece
Luca <ca.lu39@yahoo.de>
2024-01-07 12:18:18 +0100

    some changes

--- signals.c
+++ signals.c
@@ -3,9 +3,10 @@
 void	sig_c(int sig)
 {
 	(void) sig;
-	write (1, "\n", 1);
-	rl_replace_line("", 0);
+	write (2, "\n", 1);
 	rl_on_new_line();
+	rl_replace_line("", 0);
+	// rl_replace_line("", 0);
 	rl_redisplay();
 	return ;
 }
