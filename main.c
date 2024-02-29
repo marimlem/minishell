3972c5c5f20fc7db9b951854f3b92cb457e815d7
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-02-29 16:43:14 +0100

    main: fixing leaks with valgrind, created com_lst_del()

--- main.c
+++ main.c
@@ -26,12 +26,61 @@ void	lex_lst_del(t_tok *lst)
 	}
 
 }
+void	com_lst_del(t_com *lst)
+{
+	t_com	*c;
+	int	i;
+
+	i = 0;
+	c = lst;
+	if (c)
+	{
+		com_lst_del(c->next);
+		if (c->file)
+			free (c->file);
+		c->file = NULL;
+		if (c->args)
+		{
+			while (c->args[i])
+			{
+				free (c->args[i]);
+				c->args[i++] = NULL;
+			}
+			free (c->args);
+			c->args = NULL;
+		}
+		i = 0;
+		if (c->rdr)
+		{
+			while (c->rdr[i])
+			{
+				free (c->rdr[i]);
+				c->rdr[i++] = NULL;
+			}
+			free (c->rdr);
+			c->rdr = NULL;
+		}
+		c->next = NULL;
+		free (c);
+	}
+
+}
 
 void	free_n_clean(t_data *d, int b)
 {
 	lex_lst_del(d->node);
+	com_lst_del(d->com);
+	
 	if (d->input)
 		free (d->input);
+	// if (d->node)
+		// free (d->node);
+	if (d->var_node)
+		free (d->var_node);
+	// if (d->com)
+		// free (d->com);
+	if (d->tmp)
+		free (d->tmp);
 	d->error = 0;
 	d->i = 0;
 	d->q = 0;
@@ -56,10 +105,12 @@ int	main(int argc, char **argv)
 		d->input = NULL;
 		d->node = NULL;
 		d->var_node = NULL;
+		d->com = NULL;
+		d->tmp = NULL;
 		d->i = 0;
 		d->q = 0;
 		d->error = 0;
-		
+
 		inputparsing(d);
 		if (d->error != 0)
 			printf("Error (%d)\n", d->error);
