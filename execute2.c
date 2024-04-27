3f8a8cb49c254016dab957c1d2aa3c79085fdba6
Luca <ca.lu39@yahoo.de>
2024-04-27 10:44:09 +0200

    executor: print error messages on stderr; rdr: output fix; rdr+pipe: fix that pipe shouldn't take output rdr

--- execute2.c
+++ execute2.c
@@ -7,9 +7,26 @@ void	execute_one(t_data *d)
 	return ;
 }
 
+void	close_rdr(t_data *d)
+{
+		if (d->old_fd[OUT] >= 0)
+		{
+			dup2(d->old_fd[OUT], 1);
+			close(d->fd[OUT]);
+		}
+		if (d->old_fd[IN] >= 0)
+		{
+			dup2(d->old_fd[IN], 0);
+			close(d->fd[IN]);
+			d->fd[IN] = open(".minishell_heredoc_tmp_file", O_TRUNC , 0644);
+			if (d->fd[IN] > 0)
+				close(d->fd[IN]);
+		}
+}
+
 int	rdr_out(t_data *d, t_com *current, int j)
 {
-	if (d->old_fd == 0)
+	if (d->old_fd[OUT] < 0) // first round
 		d->old_fd[OUT] = dup(STDOUT_FILENO);
 	else
 		close(d->fd[OUT]);
@@ -19,19 +36,21 @@ int	rdr_out(t_data *d, t_com *current, int j)
 		d->fd[OUT] = open(current->rdr[j + 1], O_WRONLY | O_CREAT | O_APPEND, 0644);
 	if(d->fd[OUT] < 0)
 	{
-		printf("rdr > : error opening file\n");
+		// printf("rdr > : error opening file\n");
+		ft_putstr_fd("rdr > : error opening file\n", 2);
 		dup2(d->old_fd[OUT], 1);
 		return (1);
 	}
+	dup2(d->fd[OUT], STDOUT_FILENO);
 	return (0);
 }
 
 int	rdr_in(t_data *d, t_com *current, int j)
 {
-	if (d->old_fd[IN] != 0)
-		close(d->fd[IN]);
-	else
+	if (d->old_fd[IN] < 0)
 		d->old_fd[IN] = dup(STDIN_FILENO);
+	else
+		close(d->fd[IN]);
 	if (current->rdr[j][1] == '<')
 		heredoc_start(d, current);
 	else
@@ -39,7 +58,8 @@ int	rdr_in(t_data *d, t_com *current, int j)
 		d->fd[IN] = open(current->rdr[j + 1], O_RDONLY);
 		if (d->fd[IN] < 0)
 		{
-			printf("rdr <: error opening file\n");
+			// printf("rdr <: error opening file\n");
+			ft_putstr_fd("rdr < : error opening file\n", 2);
 			dup2(d->old_fd[IN], 0);
 			return (1);
 		}
@@ -91,20 +111,29 @@ int	rdr_handler(t_data *d, t_com *current)
 		j = j + 2;
 
 	}
+
 	return (0);
 
 }
 
 void	pipe_handler(t_data *d, int pc, int i)
 {
+	// printf("stdout: %d ... stdin: %d\n", STDOUT_FILENO, STDIN_FILENO);
+	// close_rdr(d);
 	if (i != pc)
 	{
-		dup2(d->p[i][1], STDOUT_FILENO);
+		if (d->old_fd[OUT] < 0)
+			dup2(d->p[i][1], STDOUT_FILENO);
+		else
+			dup2(d->p[i][1], d->old_fd[OUT]);
 		close_pipes(d->p[i]);
 	}
 	if ( i != 0)
 	{
-		dup2(d->p[i-1][0], STDIN_FILENO);
+		if (d->old_fd[IN] < 0)
+			dup2(d->p[i-1][0], STDIN_FILENO);
+		else
+			dup2(d->p[i-1][0], d->old_fd[IN]);
 		close_pipes(d->p[i-1]);
 	}
 }
@@ -115,11 +144,15 @@ void	playground(t_data *d, t_com *current ,int pc, int i)
 
 	if (current->rdr)
 		rdr_handler(d, current);
-	pipe_handler(d, pc, i);
+	if (pc != 0)
+		pipe_handler(d, pc, i);
 
 	if (execve(current->file, current->args, NULL) == -1)
 	{
-		printf("minishell: command not found: %s\n",current->args[0]); //file[9]);
+		ft_putstr_fd("minishell: command not found: ", 2);
+		ft_putstr_fd(current->args[0], 2);
+		ft_putstr_fd("\n", 2);
+		// printf("minishell: command not found: %s\n",current->args[0]); //file[9]);
 		free_n_clean(d, 1);
 		exit(-1);
 	}
@@ -134,7 +167,7 @@ void	process_handler(t_data *d, t_com *current, int pc, int i)
 		return ;
 	free (current->file);
 	current->file = file;
-	if (i != pc)
+	if (pc != 0 && i != pc)
 		pipe(d->p[i]);
 	d->pid = fork();
 	if (d->pid < 0)
@@ -146,7 +179,7 @@ void	process_handler(t_data *d, t_com *current, int pc, int i)
 	}
 	else
 	{
-		if (i != 0)
+		if (pc != 0 && i != 0)
 			close_pipes(d->p[i - 1]);
 		waitpid(d->pid, &(d->status), 0);
 	}
@@ -164,10 +197,12 @@ void	execute_loop(t_data *d, int pc)
 	while (current)
 	{
 		process_handler(d, current, pc, i);
+		close_rdr(d);
 		i++;
 		current = current->next;
 		d->status = 0;
 		d->pid = 0;
+
 	}
 }
 
@@ -179,12 +214,12 @@ void	executor2(t_data *d)
 	if (setup_fds(d) != 0)
 		return ; //alloc error
 	count = d_lstsize(d->com);
-	printf("fd: %d ... count: %d\n", d->fd[0], count);
-	if (count == 1)
-		execute_one(d);
-	else if (count > 1)
+	// printf("fd: %d ... count: %d\n", d->fd[0], count);
+	// if (count == 1)
+	// 	execute_one(d);
+	if (count > 0)
 	{
-		if (setup_pipes(d, count - 1) != 0)
+		if (count > 1 && setup_pipes(d, count - 1) != 0)
 			return ; // alloc error
 		execute_loop(d, count - 1);
 	}
