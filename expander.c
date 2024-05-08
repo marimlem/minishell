635d9786871bf542a6326a1fd1a95bf65804aa24
Luca <ca.lu39@yahoo.de>
2024-05-08 11:57:30 +0200

    exit code expansion OK

--- expander.c
+++ expander.c
@@ -62,8 +62,45 @@ void 	expand_empty(t_data *d, char *new)
 
 void 	expand_exitstatus(t_data *d) // not as simple unfortunately, will need to allocate space for longer numbers
 {
-	d->tmp[d->i++] = '0';
-	ft_memmove(&d->tmp[d->i], &d->tmp[d->i+1], ft_strlen(&d->tmp[d->i + 1]) + 1);
+	// d->tmp[d->i++] = '0';
+	// ft_memmove(&d->tmp[d->i], &d->tmp[d->i+1], ft_strlen(&d->tmp[d->i + 1]) + 1);
+
+	char	*code;
+	char	*exp;
+	int		len;
+
+	code = ft_itoa(d->exit_code);
+	if (code == NULL)
+	{
+		// alloc error
+		return ;
+	}
+	len = ft_strlen(code);
+	exp = (char *) ft_calloc(len + ft_strlen(d->tmp), sizeof(char));
+	if (exp ==NULL)	
+	{
+		// alloc error
+		return ;
+	}
+	// ft_memmove(new, d->tmp, ft_strlen(d->i));
+
+	ft_memmove(exp, d->tmp, d->i);
+	ft_memmove(&exp[d->i], code, len);
+	if (d->tmp[d->i + 2] != 0)
+	{
+		ft_memmove(&exp[ft_strlen(code) + d->i], &d->tmp[d->i + 2], ft_strlen(&d->tmp[d->i + 2]));
+	}
+	else
+	{
+		while (d->tmp[d->i + len])
+		{
+			d->tmp[d->i + len] = 0 ;
+			len ++;
+		}
+	}
+	d->i = d->i + ft_strlen(code);
+	free(d->tmp);
+	d->tmp = exp;
 	return ;
 }
 
