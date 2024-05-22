9adcb4722352b411a52df40b68ba417681386ffd
Luca <ca.lu39@yahoo.de>
2024-05-22 20:16:18 +0200

    mostly refactored; manual testing OK, tester seems also OK

--- lexer.c
+++ lexer.c
@@ -9,106 +9,146 @@ int	lex_is_separator(char c)
 	return (0);
 }
 
+// quote handling: start quote; end quote; loop through quoted areas
+int	lexer_quote_h(t_data *d,t_tok *current)
+{
+	if (d->q == 0 && (current->tok[d->i] == DBLQUOTE || current->tok[d->i] == SGLQUOTE))
+	{
+		d->q = current->tok[d->i];
+		d->i++;
+		return (1);
+	}
+	else if (d->q != 0 && current->tok[d->i] == d->q)
+	{
+		d->q = 0;
+		d->i++;
+		return (1);
+	}
+	else if (d->q != 0)
+	{
+		d->i++;
+		return (1);
+	}
+	return (0);
+}
+
+int	lex_sep_firstword(t_data *d, t_tok **current)
+{
+	while (lex_is_separator((*current)->tok[d->i]) == 1)
+	{
+		(*current)->tok[d->i] = 0;
+		d->i++;
+	}
+	if ((*current)->tok[d->i] == 0)
+		return (2);
+	if (d->i != 0)
+	{
+		ft_memmove((*current)->tok, &(*current)->tok[d->i], ft_strlen(&(*current)->tok[d->i]) + 1);
+	}
+	d->i = 0; //not sure about this; maybe = 0? (used to be =1)
+	return (1);
+}
+
+int	lex_sep_words(t_data *d, t_tok **current)
+{
+	while (lex_is_separator((*current)->tok[d->i]) == 1)
+	{
+		(*current)->tok[d->i] = 0;
+		d->i++;
+	}
+	if ((*current)->tok[d->i] == 0)
+		return (2);
+	lex_lstsqueezein(&(*current), &(*current)->tok[d->i]);
+	(*current) = (*current)->next;
+	d->i = 0;
+	return (1);
+
+}
+
+int	lex_sep_special(t_data *d, t_tok **current)
+{
+	if (d->i != 0)
+	{
+		lex_lstsqueezein(&(*current), &(*current)->tok[d->i]);
+		(*current)->tok[d->i] = 0;
+		(*current) = (*current)->next;
+		d->i = 1;
+		if ((*current)->tok[0] == '|' && ((*current)->tok[1] == '<' || (*current)->tok[1] == '>'))
+		{
+			lex_lstsqueezein(&(*current), &(*current)->tok[d->i]);
+			(*current)->tok[d->i] = 0;
+			(*current) = (*current)->next;
+			d->i = 1;
+		}
+	}
+	if ((*current)->tok[d->i] == '|' && ((*current)->tok[d->i+1] == '<' || (*current)->tok[d->i+1] == '>'))
+	{
+		d->i++;
+		lex_lstsqueezein(&(*current), &(*current)->tok[d->i]);
+		(*current)->tok[d->i] = 0;
+		(*current) = (*current)->next;
+		d->i = 1;
+	}
+	while (lex_is_separator((*current)->tok[d->i]) == 2)
+	{
+		d->i++;
+	}
+	while (lex_is_separator((*current)->tok[d->i]) == 1)
+	{
+		(*current)->tok[d->i] = 0;
+		d->i++;
+	}
+	if ((*current)->tok[d->i])
+	{
+		lex_lstsqueezein(&(*current), &(*current)->tok[d->i]);
+		(*current)->tok[d->i] = 0;
+		(*current) = (*current)->next;
+		d->i = 0;
+	}
+	return (1);
+
+}
+
+int	lexer_separator(t_data *d, t_tok **current)
+{
+	if (d->q == 0 && lex_is_separator((*current)->tok[d->i]) == 1 && d->i == 0)
+	{
+		return (lex_sep_firstword(d, current));
+	}
+	else if (d->q == 0 && lex_is_separator((*current)->tok[d->i]) == 1)
+	{
+		return (lex_sep_words(d, current));
+	}
+	else if (d->q == 0 && lex_is_separator((*current)->tok[d->i]) == 2)
+	{
+		return (lex_sep_special(d, current));
+	}
+	return (0);
+}
+
+
 void	lexer(t_data *d)
 {
 	t_tok *current;
-	//new try:
+	int	s;
+	
 	init_list2(d, d->input);
 	current = d->node;
 	d->i = 0;
+	s = 0;
 	while (current && current->tok)
 	{
 		if (current->tok[d->i] == 0)
 			break;
-		else if (d->q == 0 && (current->tok[d->i] == DBLQUOTE || current->tok[d->i] == SGLQUOTE))
-		{
-			d->q = current->tok[d->i];
-			d->i++;
-		}
-		else if (d->q != 0 && current->tok[d->i] == d->q)
-		{
-			d->q = 0;
-			d->i++;
-		}
-		else if (d->q != 0)
-			d->i++;
-		else if (d->q == 0 && lex_is_separator(current->tok[d->i]) == 1 && d->i == 0)
-		{
-			while (lex_is_separator(current->tok[d->i]) == 1)
-			{
-				current->tok[d->i] = 0;
-				d->i++;
-			}
-			if (current->tok[d->i] == 0)
-				break;
-			if (d->i != 0)
-			{
-				ft_memmove(current->tok, &current->tok[d->i], ft_strlen(&current->tok[d->i]) + 1);
-			}
-			d->i = 0; //not sure about this; maybe = 0? (used to be =1)
-		}
-
-		else if (d->q == 0 && lex_is_separator(current->tok[d->i]) == 1)
-		{
-			while (lex_is_separator(current->tok[d->i]) == 1)
-			{
-				current->tok[d->i] = 0;
-				d->i++;
-			}
-			if (current->tok[d->i] == 0)
-				break;
-			lex_lstsqueezein(&current, &current->tok[d->i]);
-			current = current->next;
-			d->i = 0;
-		}
-		else if (d->q == 0 && lex_is_separator(current->tok[d->i]) == 2)
-		{
-			if (d->i != 0)
-			{
-				lex_lstsqueezein(&current, &current->tok[d->i]);
-				current->tok[d->i] = 0;
-				current = current->next;
-				d->i = 1;
-				if (current->tok[0] == '|' && (current->tok[1] == '<' || current->tok[1] == '>'))
-				{
-					lex_lstsqueezein(&current, &current->tok[d->i]);
-					current->tok[d->i] = 0;
-					current = current->next;
-					d->i = 1;
-				}
-			}
-			if (current->tok[d->i] == '|' && (current->tok[d->i+1] == '<' || current->tok[d->i+1] == '>'))
-			{
-				d->i++;
-				lex_lstsqueezein(&current, &current->tok[d->i]);
-				current->tok[d->i] = 0;
-				current = current->next;
-				d->i = 1;
-			}
-			while (lex_is_separator(current->tok[d->i]) == 2)
-			{
-				d->i++;
-			}
-			while (lex_is_separator(current->tok[d->i]) == 1)
-			{
-				current->tok[d->i] = 0;
-				d->i++;
-			}
-			if (current->tok[d->i])
-			{
-				lex_lstsqueezein(&current, &current->tok[d->i]);
-				current->tok[d->i] = 0;
-				current = current->next;
-				d->i = 0;
-
-			}
-
-		}
-		else if (d->q == 0)
-			d->i++;
-
+		if (lexer_quote_h(d, current) == 1)
+			continue ;
+		s = lexer_separator(d, &current);
+		if (s == 1)
+			continue ;
+		else if (s == 2)
+			break ;
+		d->i++;
 	}
-	// lst_print(d->node);
 }
 
 // check for unclosed quotes, error if odd number
