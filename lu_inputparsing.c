c577738d4f2fd90ffe17f911775d6cf51eeb8010
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-05-31 16:05:33 +0200

    rm comments

--- lu_inputparsing.c
+++ lu_inputparsing.c
@@ -11,44 +11,11 @@ void	inputparsing(t_data *d)
 		exit(0);
 	}
 	signal_setup(d, MODE_DF);
-
-
-/* 	if (isatty(fileno(stdin)))
-	{
-
-		d->input = readline("minishell:$ ");
-		if (!d->input)
-		{
-			ft_putstr_fd("exit\n", 1);
-			free_n_clean(d, 1);
-			exit(0);
-		}
-	}
-	else
-	{
-		char *line;
-		line = get_next_line(fileno(stdin));
-		d->input = ft_strtrim(line, "\n");
-		free(line);
-	} */
 	add_history(d->input);
-
 	if (!d || !d->input)
 		return ;
 	if (even_quotes(d) == ODD)
 		return ;
-	// if (ft_strncmp(d->input, "exit", 4) == 0)
-	// {
-	// 	d->error = -1;
-	// 	return ;
-	// }
-
 	lexer(d);
 	parser(d);
-	// if (d->com && d->com->args && ft_strcmp(d->com->args[0], "env") == 0)
-	// 	ft_print_list(*envlist);
-		// printf("even!");
-
-	//parser
-
 }
