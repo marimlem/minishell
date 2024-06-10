dd4fd04720aeb086c1fd06462c090d52c8767f3e
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-06-10 19:46:34 +0200

    norm: cleanup

--- tok_types.c
+++ tok_types.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tok_types.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lknobloc <lknobloc@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 19:37:34 by lknobloc          #+#    #+#             */
+/*   Updated: 2024/06/10 19:46:25 by lknobloc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // finds and marks operator tokens
@@ -8,23 +20,26 @@ void	p_op_type(t_data *d)
 	int		t;
 
 	current = d->node;
-	t=1;
+	t = 1;
 	d->i = 0;
 	while (current && current->tok)
 	{
-		if ((current->tok[0] == '>' || current->tok[0] == '<' || current->tok[0] == '|'))
+		if ((current->tok[0] == '>' || current->tok[0] == '<'
+				|| current->tok[0] == '|'))
 		{
 			if (ft_strlen(current->tok) > 2)
 			{
 				d->error = ERR_PAR_SYN;
 				return ;
 			}
-			else if ((current->tok[1] && current->tok[0] == '|') || (current->tok[1] && current->tok[0] != current->tok[1]))
+			else if ((current->tok[1] && current->tok[0] == '|')
+				|| (current->tok[1] && current->tok[0] != current->tok[1]))
 			{
 				d->error = ERR_PAR_SYN;
 				return ;
 			}
-			else if (ft_strlen(current->tok) == 1 || (current->tok[1] && current->tok[0] == current->tok[1]))
+			else if (ft_strlen(current->tok) == 1 || (current->tok[1]
+					&& current->tok[0] == current->tok[1]))
 			{
 				if (current->tok[0] == '|')
 					t = 1;
@@ -42,35 +57,32 @@ void	p_op_type(t_data *d)
 	}
 }
 
-
-
-// checks if operators are in syntactically good order (not first, not last, not two in a row)
+// checks if operators are in syntactically good order 
+	// (not first, not last, not two in a row)
 // pipe cant be first or last
 // before a pipe, no other operator can be there ( > |  or | |)
-// two in a row check rdr except pipe followed by rdr; rdr must be followed by normal type token
+// two in a row check rdr except pipe followed by rdr; rdr must be followed
+	// by normal type token
 // last token cant be an operator
 void	p_syn_check(t_data *d)
 {
-	t_tok *current;
-	int	t;
-	int	last;
+	t_tok	*current;
+	int		t;
+	int		last;
 
 	t = 0;
 	last = 0;
 	current = d->node;
 	while (current && current->tok)
 	{
-		if (t == 0 && current->typ == '|' * (-1))
+		if ((t == 0 && current->typ == '|' * (-1))
+			|| (current->typ == '|' * (-1) && last < -20))
 		{
 			d->error = ERR_PAR_SYN;
 			return ;
 		}
-		else if (current->typ == '|' * (-1) && last < -20)
-		{
-			d->error = ERR_PAR_SYN;
-			return ;
-		}
-		else if ((current->typ == '>' * (-1) || current->typ == '<' * (-1)) && (last == '>' * (-1) || last == '<' * (-1)))
+		else if ((current->typ == '>' * (-1) || current->typ == '<' * (-1))
+			&& (last == '>' * (-1) || last == '<' * (-1)))
 		{
 			d->error = ERR_PAR_SYN;
 			return ;
@@ -79,7 +91,6 @@ void	p_syn_check(t_data *d)
 		t++;
 		current = current->next;
 	}
-	if (last < -10) //what about var
+	if (last < -10)
 		d->error = ERR_PAR_SYN;
 }
-
