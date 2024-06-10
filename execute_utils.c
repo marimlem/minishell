ea5cf853067ac66c2d82a36d3dda53a501d92e95
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-06-10 19:37:11 +0200

    norm: execute_utils

--- execute_utils.c
+++ execute_utils.c
@@ -1,21 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   execute_utils.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lknobloc <lknobloc@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/10 19:35:13 by lknobloc          #+#    #+#             */
+/*   Updated: 2024/06/10 19:36:54 by lknobloc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 int	setup_fds(t_data *d)
 {
-	d->old_fd = (int*) ft_calloc(sizeof(int), 3);
-	if (d->old_fd ==NULL)
+	d->old_fd = (int *) ft_calloc(sizeof(int), 3);
+	if (d->old_fd == NULL)
 		return (1);
-	d->fd = (int*) ft_calloc(sizeof(int), 3);
-	if (d->fd ==NULL)
+	d->fd = (int *) ft_calloc(sizeof(int), 3);
+	if (d->fd == NULL)
 		return (1);
 	d->fd[IN] = dup(STDIN_FILENO);
 	d->fd[OUT] = dup(STDOUT_FILENO);
-
-	d->old_fd[IN] = -1;  
+	d->old_fd[IN] = -1;
 	d->old_fd[OUT] = -1;
-
-	// d->old_fd[IN] = dup(STDIN_FILENO); //need this in rdr_handler to see if it's the first 
-	// d->old_fd[OUT] = dup(STDOUT_FILENO);
 	return (0);
 }
 
@@ -24,13 +32,13 @@ int	setup_pipes(t_data *d, int pipecount)
 	int	i;
 
 	i = 0;
-	d->p = (int **) ft_calloc(sizeof(int*), pipecount + 1);
+	d->p = (int **) ft_calloc(sizeof(int *), pipecount + 1);
 	if (d->p == NULL)
-		return (1); //alloc error
+		return (1);
 	while (i != pipecount)
 	{
 		d->p[i] = (int *) ft_calloc(sizeof(int), 2);
-		if (d->p[i] ==NULL)
+		if (d->p[i] == NULL)
 			return (1);
 		i++;
 	}
@@ -47,13 +55,13 @@ void	pipe_handler(t_data *d, int pc, int i)
 			dup2(d->p[i][1], d->old_fd[OUT]);
 		close_pipes(d->p[i]);
 	}
-	if ( i != 0)
+	if (i != 0)
 	{
 		if (d->old_fd[IN] < 0)
-			dup2(d->p[i-1][0], STDIN_FILENO);
+			dup2(d->p[i - 1][0], STDIN_FILENO);
 		else
-			dup2(d->p[i-1][0], d->old_fd[IN]);
-		close_pipes(d->p[i-1]);
+			dup2(d->p[i - 1][0], d->old_fd[IN]);
+		close_pipes(d->p[i - 1]);
 	}
 }
 
