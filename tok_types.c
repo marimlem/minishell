37227361252d0b2c297f88352b059455cfa4b4df
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-06-11 12:24:58 +0200

    refactor: tok_op_handling

--- tok_types.c
+++ tok_types.c
@@ -6,11 +6,38 @@
 /*   By: lknobloc <lknobloc@student.42vienna.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/10 19:37:34 by lknobloc          #+#    #+#             */
-/*   Updated: 2024/06/10 19:46:25 by lknobloc         ###   ########.fr       */
+/*   Updated: 2024/06/11 12:23:58 by lknobloc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+int	tok_op_handling(t_data *d, t_tok **current, int *t)
+{
+	if (!*current)
+		return (0);
+	if (ft_strlen((*current)->tok) > 2)
+	{
+		d->error = ERR_PAR_SYN;
+		return (1);
+	}
+	else if (((*current)->tok[1] && (*current)->tok[0] == '|')
+		|| ((*current)->tok[1] && (*current)->tok[0] != (*current)->tok[1]))
+	{
+		d->error = ERR_PAR_SYN;
+		return (1);
+	}
+	else if (ft_strlen((*current)->tok) == 1 || ((*current)->tok[1]
+			&& (*current)->tok[0] == (*current)->tok[1]))
+	{
+		if ((*current)->tok[0] == '|')
+			*t = 1;
+		(*current)->typ = (*current)->tok[0] * (-1);
+		(*current) = (*current)->next;
+		return (2);
+	}
+	return (0);
+}
 
 // finds and marks operator tokens
 // pipe = -124; rdr left = -60; rdr right = -62
@@ -18,41 +45,25 @@ void	p_op_type(t_data *d)
 {
 	t_tok	*current;
 	int		t;
+	int		p;
 
 	current = d->node;
 	t = 1;
 	d->i = 0;
+	p = 0;
 	while (current && current->tok)
 	{
 		if ((current->tok[0] == '>' || current->tok[0] == '<'
 				|| current->tok[0] == '|'))
 		{
-			if (ft_strlen(current->tok) > 2)
-			{
-				d->error = ERR_PAR_SYN;
+			p = tok_op_handling(d, &current, &t);
+			if (p == 1)
 				return ;
-			}
-			else if ((current->tok[1] && current->tok[0] == '|')
-				|| (current->tok[1] && current->tok[0] != current->tok[1]))
-			{
-				d->error = ERR_PAR_SYN;
-				return ;
-			}
-			else if (ft_strlen(current->tok) == 1 || (current->tok[1]
-					&& current->tok[0] == current->tok[1]))
-			{
-				if (current->tok[0] == '|')
-					t = 1;
-				current->typ = current->tok[0] * (-1);
-				current = current->next;
+			else if (p == 2)
 				continue ;
-			}
 		}
 		else
-		{
-			current->typ = t;
-			t++;
-		}
+			current->typ = t++;
 		current = current->next;
 	}
 }
