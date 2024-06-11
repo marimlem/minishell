f8196064268094604dbc6edecb04d563994f08b9
Luca Marie Knobloch <lknobloc@c2r2p1.42vienna.com>
2024-06-11 14:36:32 +0200

    norm: cleanup

--- lexer.c
+++ lexer.c
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lknobloc <lknobloc@student.42vienna.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/11 14:35:19 by lknobloc          #+#    #+#             */
+/*   Updated: 2024/06/11 14:36:23 by lknobloc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 // quote handling: start quote; end quote; loop through quoted areas
-int	lexer_quote_h(t_data *d,t_tok *current)
+int	lexer_quote_h(t_data *d, t_tok *current)
 {
-	if (d->q == 0 && (current->tok[d->i] == DBLQUOTE || current->tok[d->i] == SGLQUOTE))
+	if (d->q == 0 && (current->tok[d->i] == DBLQUOTE
+			|| current->tok[d->i] == SGLQUOTE))
 	{
 		d->q = current->tok[d->i];
 		d->i++;
@@ -25,9 +38,9 @@ int	lexer_quote_h(t_data *d,t_tok *current)
 
 void	lexer(t_data *d)
 {
-	t_tok *current;
-	int	s;
-	
+	t_tok	*current;
+	int		s;
+
 	init_list2(d, d->input);
 	current = d->node;
 	d->i = 0;
@@ -35,7 +48,7 @@ void	lexer(t_data *d)
 	while (current && current->tok)
 	{
 		if (current->tok[d->i] == 0)
-			break;
+			break ;
 		if (lexer_quote_h(d, current) == 1)
 			continue ;
 		s = lexer_separator(d, &current);
